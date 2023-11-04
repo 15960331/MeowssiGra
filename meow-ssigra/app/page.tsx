0ffaa9d0de58ba7aa037c3676b60ce6074a69b75
15960331 <84163210+15960331@users.noreply.github.com>
2023-11-04 03:29:47 +0000

    feat: add Button and Img

--- meow-ssigra/app/page.tsx
+++ meow-ssigra/app/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+
+import { Button, Img } from '@chakra-ui/react';
+
 export default function Home() {
+  // TODO: load a folder with multiple png files
+  const handleClick = () => {};
+
   return (
     <main>
-      <div>test</div>
+      <Img
+        src=""
+        alt=""
+      />
+      <Button onClick={handleClick}>Load</Button>
     </main>
   );
 }
