b609858a4cacbd535c01d73386a5d1e51a220b21
15960331 <84163210+15960331@users.noreply.github.com>
2023-12-06 23:25:34 +0900

    Feat/add-shortcut-keys (#9)
    * fix: current page number
    
    * refact: put every children in header component
    
    * feat: add shortcut keys

--- meow-ssigra/components/Header.tsx
+++ meow-ssigra/components/Header.tsx
@@ -1,11 +1,46 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex, IconButton } from '@chakra-ui/react';
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
+
 import { HEADER_HEIGHT } from '@/constants';
+import { FitMode, ViewMode } from '@/types';
+
+import { FileInput } from './FileInput';
+import { PageCounter } from './PageCounter';
 
 type Props = {
-  children: React.ReactElement[];
+  fileProps: {
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  };
+  viewModeProps: {
+    viewMode: ViewMode;
+    onClick: () => void;
+  };
+  fitModeProps: {
+    fitMode: FitMode;
+    onClick: () => void;
+  };
+  backProps: {
+    disabled: boolean;
+    onClick: () => void;
+  };
+  forwardProps: {
+    disabled: boolean;
+    onClick: () => void;
+  };
+  pageCounterProps: {
+    currentPage: number;
+    totalPage: number;
+  };
 };
 
-export const Header: React.FC<Props> = ({ children }) => {
+export const Header: React.FC<Props> = ({
+  fileProps,
+  viewModeProps,
+  fitModeProps,
+  backProps,
+  forwardProps,
+  pageCounterProps,
+}) => {
   return (
     <header>
       <Flex
@@ -17,7 +52,40 @@ export const Header: React.FC<Props> = ({ children }) => {
         alignItems="center"
         gap="8px"
       >
-        {children}
+        <FileInput onChange={fileProps.onChange} />
+        <Button
+          size="sm"
+          width={100}
+          isDisabled
+          onClick={viewModeProps.onClick}
+        >
+          {viewModeProps.viewMode === 'normal' ? 'Normal' : 'Manga'}
+        </Button>
+        <Button
+          size="sm"
+          width={100}
+          onClick={fitModeProps.onClick}
+        >
+          {fitModeProps.fitMode === 'vertical' ? 'Vertical' : 'Horizontal'}
+        </Button>
+        <IconButton
+          aria-label="back button"
+          size="sm"
+          icon={<ArrowBackIcon />}
+          isDisabled={backProps.disabled}
+          onClick={backProps.onClick}
+        />
+        <IconButton
+          aria-label="forward button"
+          size="sm"
+          icon={<ArrowForwardIcon />}
+          isDisabled={forwardProps.disabled}
+          onClick={forwardProps.onClick}
+        />
+        <PageCounter
+          currentPage={pageCounterProps.currentPage}
+          totalPage={pageCounterProps.totalPage}
+        />
       </Flex>
     </header>
   );
